e1c8c258e1f1f4c3a5c9ee12e5d1acaa1dc37f26
Mohammed Mohiuddin <134163617+Mohiuddin2204@users.noreply.github.com>
2025-01-23 10:23:29 +0530

    Add files via upload

---
+++ springmavenmohi/src/main/java/com/maven/mohi/MohiApplication.java
@@ -0,0 +1,13 @@
+package com.maven.mohi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohiApplication.class, args);
+	}
+
+}
