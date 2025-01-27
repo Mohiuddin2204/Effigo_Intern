98b45aceb030d855a9be2accb6b6c32cf1c40a83
Mohammed Mohiuddin <134163617+Mohiuddin2204@users.noreply.github.com>
2025-01-27 15:37:09 +0530

    Add files via upload

---
+++ firstwebapplication/src/test/java/com/mohi/springboot/firstwebapplication/FirstwebapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohi.springboot.firstwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstwebapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ firstwebapplication/target/classes/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>hellooo</title>
+	<body>
+		<h1>heading hello</h1>
+		</body>
+
+
+</html>
