8a7acf26a8b22613c1cc7a78d6caeadff3089220
Mohammed Mohiuddin <134163617+Mohiuddin2204@users.noreply.github.com>
2025-02-07 11:25:59 +0530

    Update PublisherJpaRepository.java

--- manytomany/src/main/java/com/springboot/manytomany/repository/PublisherJpaRepository.java
+++ manytomany/src/main/java/com/springboot/manytomany/repository/PublisherJpaRepository.java
@@ -3,9 +3,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.springboot.manytomany.model.Publisher;
-
+import java.util.Optional;
 
 @Repository
 public interface PublisherJpaRepository extends JpaRepository<Publisher,Integer>{
     
+	Optional<Publisher> findByPublisherName(String publisherName); // Custom method
 }
