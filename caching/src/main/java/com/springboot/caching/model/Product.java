70d5d25f48a441fb20d7096fbc16c2d84a6ea5fd
Mohammed Mohiuddin <134163617+Mohiuddin2204@users.noreply.github.com>
2025-02-20 15:59:09 +0530

    Update Product.java

--- caching/src/main/java/com/springboot/caching/model/Product.java
+++ caching/src/main/java/com/springboot/caching/model/Product.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Product {
+public class Product implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
